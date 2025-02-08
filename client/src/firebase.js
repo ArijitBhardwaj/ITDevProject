28c3dc21a24ae3531516fd612cc243698f7321e7
Kiranpreet Kaur <000475341@student.vcc.ca>
2025-02-08 02:33:52 -0800

    Added Components for SignIn and SignUp, alongwith setting Firebase

---
+++ client/src/firebase.js
@@ -0,0 +1,39 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAM6lOsZOhM6E1dmcL-Bncb-htqiIwJ0Ho",
+    authDomain: "itdevproject-bc92d.firebaseapp.com",
+    projectId: "itdevproject-bc92d",
+    storageBucket: "itdevproject-bc92d.firebasestorage.app",
+    messagingSenderId: "769392675700",
+    appId: "1:769392675700:web:afb6ce86da72457d7aba21",
+    measurementId: "G-4C8ME8TN0S"
+  };
+
+const app = firebase.initializeApp(firebaseConfig);
+const auth = app.auth();
+
+
+export { auth, firebase };
+
+
+
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyAM6lOsZOhM6E1dmcL-Bncb-htqiIwJ0Ho",
+//     authDomain: "itdevproject-bc92d.firebaseapp.com",
+//     projectId: "itdevproject-bc92d",
+//     storageBucket: "itdevproject-bc92d.firebasestorage.app",
+//     messagingSenderId: "769392675700",
+//     appId: "1:769392675700:web:afb6ce86da72457d7aba21",
+//     measurementId: "G-4C8ME8TN0S"
+//   };
+  
+//   // Initialize Firebase
+//   const app = initializeApp(firebaseConfig);
+//   const analytics = getAnalytics(app);
+
+//   analytics.logEvent('notification_received');
