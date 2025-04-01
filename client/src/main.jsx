b52ce645b4f4b91808226fdcdb86249370a4ec42
gatar <gatarezakayo@gmail.com>
2025-03-31 18:47:41 -0700

    Added fully functional UserProfile with navigation and CSS modularization

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -17,14 +17,37 @@
 //   </StrictMode>,
 // )
 
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import { BrowserRouter } from "react-router-dom";
+// import App from "./App";
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <BrowserRouter>
+//     <App />
+//   </BrowserRouter>
+// );
+
+// main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './Pages/HomePage';
+import UserProfilePage from './Pages/UserProfilePage';
+import NavigationPage from './Pages/NavigationPage';
+
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/profile" element={<UserProfilePage />} />
+        <Route path="/navigate" element={<NavigationPage />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 );
-
