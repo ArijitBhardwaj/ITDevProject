fd17645260ace64a7119d147dff7865375388366
Aiden Wu <wuazhihong@gmail.com>
2025-04-01 21:15:29 -0700

    Changed map file to contain testing environment data. Example map data and sample data JSONs are required to fully run the file. The map now renders a jsx line properly as well as navigation coordinates.

--- client/src/Pages/Map.jsx
+++ client/src/Pages/Map.jsx
@@ -1,14 +1,17 @@
 import React, { useState } from "react"
 import { Box, Typography } from "@mui/material"
 import { useRef, useEffect } from "react"
-import mapImage from '../../assets/vcc_floor1.jpg'
+import {TransformWrapper, TransformComponent} from "react-zoom-pan-pinch"
+import mapImage from '../../assets/vcc_floor1_grid.png'
 import './Map.css'
 import mapData from '../../testing/sampleData.json'
+import traversableData from '../../testing/traversable.json'
 
 const Map = () =>
 {
 
     const [pathCoordinates, setPathCoordinates] = useState([])
+    const [traversableCoordinates, setTraversableCoordinates] = useState([])
 
 
     useEffect(() => {
@@ -18,6 +21,11 @@ const Map = () =>
             return [node.x, node.y]
         }))
 
+
+        setTraversableCoordinates(traversableData.map(node =>
+        {            
+            return [node.coordinates.x, node.coordinates.y]
+        }))
         // Output testing
         // console.log(pathCoordinates)
 
@@ -30,13 +38,31 @@ const Map = () =>
 
     // console.log(pathCoordinates)
 
-    function scalePathCoordinatesForImage()
-    {                
-        return pathCoordinates.map(node =>{
-            node[0] += 256
-            node[1] += 256            
-        })
+    function scaleTraversableCoordinatesForImage()
+    {      
+        const temp = traversableCoordinates.map(node =>{                      
+            node[0] = (node[0] * (256/20)) + 256
+            node[1] = (node[1] * (256/20)) + 256
+            
+            return node
+        })        
+        return temp
     }
+
+    function scalePathCoordinatesForImage()
+    {      
+        const temp = pathCoordinates.map(node =>{
+            console.log(node[0], node[1])            
+            node[0] = (node[0] * (256/20)) + 256
+            node[1] = (node[1] * (256/20)) + 256
+            
+            return node
+        })
+        console.log(temp)
+        return temp
+    }
+
+    // console.log(scalePathCoordinatesForImage())
 
     return (
         <>
@@ -52,24 +78,57 @@ const Map = () =>
                     </>
                 ))}                
             </Box>
-            <Box 
-            sx={{
-                backgroundImage: `url(${mapImage})`,
-                backgroundRepeat: "no-repeat",
-                backgroundSize: "512px 512px",                                                
-            }}>
-            <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="renderedPath"
-                    width={512}
-                    height={512}
-                >
-                    <polyline
-                        points={[[256,256],[0,0]]}
-                        style={{fill:"none", stroke:"black", width:3}}
-                    />
-                </svg>
-            </Box>
+            <TransformWrapper>
+                <TransformComponent>
+                    <Box 
+                    sx={{
+                        backgroundImage: `url(${mapImage})`,
+                        backgroundRepeat: "no-repeat",
+                        backgroundSize: "512px 512px",                                                
+                    }}>
+                    <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="renderedPath"
+                            width={512}
+                            height={512}
+                        >
+                            <polyline
+                                points={scalePathCoordinatesForImage()}
+                                style={{fill:"none", stroke:"red", width:3}}
+                            />
+                        </svg>
+                    </Box>
+                </TransformComponent>
+            </TransformWrapper>
+            <TransformWrapper>
+                <TransformComponent>
+                    <Box 
+                    sx={{
+                        backgroundImage: `url(${mapImage})`,
+                        backgroundRepeat: "no-repeat",
+                        backgroundSize: "512px 512px",                                                
+                    }}>
+                    <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="renderedPath"
+                            width={512}
+                            height={512}
+                        >
+                            {scaleTraversableCoordinatesForImage().map((node, index) =>
+                        (
+                            <>
+                                <circle
+                                    cx={node[0]} cy={node[1]}
+                                    // width={12} height={12}
+                                    r={6}
+                                    style={{fill:"orange", stroke:"blue"}}
+                                />
+                            </>
+                        ))}
+                        </svg>
+                    </Box>
+                </TransformComponent>
+            </TransformWrapper>
         </>        
     )
 }
