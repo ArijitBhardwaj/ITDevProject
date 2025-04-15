ac4b7ac61f0a591deff68b280d486eb5ed9888a6
Aiden Wu <wuazhihong@gmail.com>
2025-04-14 22:14:39 -0700

    Getting data through the console log for testing

--- client/src/Pages/MapView.jsx
+++ client/src/Pages/MapView.jsx
@@ -171,7 +171,7 @@ export default function MapView({
           parseFloat(node.coordinates.y) * (256 / 20) * -1 + 256;
         return node;
       });
-      
+      console.log(temp)
       if (temp.length > 0) setScaledFlag(true);
       return temp;
     } else {
